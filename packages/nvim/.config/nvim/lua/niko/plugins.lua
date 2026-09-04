6a5645f07c0f3ef3e2fdced182b13a587e594bd4
Niko Carpenter <niko@nikocarpenter.com>
2026-09-04 13:45:26 -0500

    Disable WhichKey spelling picker

--- packages/nvim/.config/nvim/lua/niko/plugins.lua
+++ packages/nvim/.config/nvim/lua/niko/plugins.lua
@@ -103,7 +103,11 @@ vim.keymap.set('n', '<leader>ff', fzf.files,     { desc = 'Find files' })
 vim.keymap.set('n', '<leader>fg', fzf.live_grep, { desc = 'Live grep' })
 vim.keymap.set('n', '<leader>fb', fzf.buffers,   { desc = 'Buffers' })
 vim.keymap.set('n', '<leader>fh', fzf.help_tags, { desc = 'Help tags' })
-require("which-key").setup({})
+require("which-key").setup({
+	plugins = {
+		spelling = { enabled = false },
+	},
+})
 require('uv').setup({})
 
 -- Remove fugitive buffers when hidden
